ad38fb8b30cf77b9b466700651a68747d3b72b9f
minnukota381 <minnukota381@gmail.com>
2024-05-11 17:41:22 +0530

    styles.css | IG

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-
-  $('#dark-mode-toggle').click(function() {
+  // Toggle dark mode
+  $('#dark-mode-toggle-small, #dark-mode-toggle-large').click(function() {
     $('body').toggleClass('dark-mode');
-    var darkModeEnabled = $('body').hasClass('dark-mode');
+    let darkModeEnabled = $('body').hasClass('dark-mode');
     if (darkModeEnabled) {
-      $(this).html('<i class="fas fa-sun"></i>');
+      $('#dark-mode-toggle-small, #dark-mode-toggle-large').html('<i class="fas fa-sun"></i>');
     } else {
-      $(this).html('<i class="fas fa-moon"></i>');
+      $('#dark-mode-toggle-small, #dark-mode-toggle-large').html('<i class="fas fa-moon"></i>');
     }
   });
 
+  // Open modal when image is clicked
   $('.image-container img').click(function() {
-    var src = $(this).attr('src');
-    $('.modal-body img').attr('src', src);
+    let src = $(this).attr('src');
+    $('#modal-image').attr('src', src);
+    $('#exampleModal').modal('show');
+  });
+
+  // Close modal when close button is clicked
+  $('.close-modal').click(function() {
+    // No need to manually hide the modal
   });
 });
