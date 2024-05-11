8142637b9df43ad2f725a47e491e8d3ccd7bcac0
minnukota381 <minnukota381@gmail.com>
2024-05-11 22:30:36 +0530

    script.js | IG

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  // Toggle dark mode
   $('#dark-mode-toggle-small, #dark-mode-toggle-large').click(function() {
     $('body').toggleClass('dark-mode');
     let darkModeEnabled = $('body').hasClass('dark-mode');
@@ -10,15 +9,12 @@ $(document).ready(function() {
     }
   });
 
-  // Open modal when image is clicked
   $('.image-container img').click(function() {
     let src = $(this).attr('src');
     $('#modal-image').attr('src', src);
     $('#exampleModal').modal('show');
   });
 
-  // Close modal when close button is clicked
   $('.close-modal').click(function() {
-    // No need to manually hide the modal
   });
 });
